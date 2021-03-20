2af71b4070371feea9462e3e2587cfd24670df06
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-03-20 07:50:54 -0300

    insert api data

--- src/components/NewTransactoinModal/index.tsx
+++ src/components/NewTransactoinModal/index.tsx
@@ -6,6 +6,8 @@ import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
+import { api } from '../../services/api';
+
 import { Container, TransactionTypeContainer, TypeButton } from './styles';
 
 interface NewTransactoinModalProps {
@@ -23,12 +25,15 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactoinMo
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    console.log({
+
+    const data = {
       title,
       value,
       category,
       type
-    });
+    };
+
+    api.post('/transactions', data)
   }
 
   return (
